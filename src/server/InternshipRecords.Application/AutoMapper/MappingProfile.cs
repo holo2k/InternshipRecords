9d282e906c832b8eb6f7d03beefd5059e458c88f
holo2k <nneketaa@yandex.ru>
2025-09-05 18:34:01 +0500

    feat: add automapper

---
+++ src/server/InternshipRecords.Application/AutoMapper/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using InternshipRecords.Application.Features.Direction;
+using InternshipRecords.Application.Features.Intern;
+using InternshipRecords.Application.Features.Project;
+using InternshipRecords.Domain.Entities;
+
+namespace InternshipRecords.Application.AutoMapper;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Direction, DirectionDto>().ReverseMap();
+        CreateMap<Intern, InternDto>().ReverseMap();
+        CreateMap<Project, ProjectDto>().ReverseMap();
+    }
+}
