5387d388e66d858785c5914ca4a7989b81cd8eae
holo2k <nneketaa@yandex.ru>
2025-09-05 18:35:31 +0500

    feat: add validation behavior

--- src/server/InternshipRecords.Web/Startup/Startup.cs
+++ src/server/InternshipRecords.Web/Startup/Startup.cs
@@ -1,5 +1,9 @@
 ﻿using System.Text.Json.Serialization;
+using FluentValidation;
+using InternshipRecords.Application.AutoMapper;
 using InternshipRecords.Infrastructure;
+using InternshipRecords.Web.PipelineBehaviors;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InternshipRecords.Web.Startup;
@@ -12,8 +16,6 @@ public static class Startup
 
         var connection = configuration.GetConnectionString("DefaultConnection");
 
-        Console.WriteLine(connection);
-
         services.AddInfrastructure(connection!);
 
         services.AddSwagger(configuration);
@@ -24,6 +26,16 @@ public static class Startup
 
         services.Configure<ApiBehaviorOptions>(options =>
             options.SuppressModelStateInvalidFilter = true);
+
+        services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+        services.AddMediatR(c => c.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+        ValidatorOptions.Global.DefaultClassLevelCascadeMode = CascadeMode.Stop;
+        ValidatorOptions.Global.DefaultRuleLevelCascadeMode = CascadeMode.Stop;
+        services.AddValidatorsFromAssemblyContaining<Program>();
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
         services.AddEndpointsApiExplorer();
 
