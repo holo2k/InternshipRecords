9a6e525492d37ceaf68b6b2685fe194529bde8a4
holo2k <nneketaa@yandex.ru>
2025-09-18 01:25:22 +0500

    feat (client) : edit frontend pages, delete dummies

--- src/client/InternshipRecords.Client/Program.cs
+++ src/client/InternshipRecords.Client/Program.cs
@@ -1,25 +1,29 @@
 using InternshipRecords.Client;
 using InternshipRecords.Client.Services;
+using InternshipRecords.Client.Services.Intern;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.SignalR.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri("http://localhost:5001/")
 });
 
-builder.Services.AddSingleton<InternService>();
-builder.Services.AddSingleton<DepartmentService>();
-builder.Services.AddSingleton<ProjectService>();
+builder.Services.AddScoped<InternService>();
+builder.Services.AddScoped<DirectionService>();
+builder.Services.AddScoped<ProjectService>();
 
+builder.Services.AddSingleton<InternSignalRService>();
 
 builder.Services.AddSingleton(sp =>
 {
     var connection = new HubConnectionBuilder()
-        .WithUrl("http://localhost:5001/chatHub")
+        .WithUrl("http://localhost:5001/internHub")
         .WithAutomaticReconnect()
         .Build();
     return connection;
